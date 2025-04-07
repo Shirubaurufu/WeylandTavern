3c37069d39be097695f294145648e92d699ebc8e
FFFox-ST-Manager <commanderbrainstorm@gmail.com>
2025-04-07 12:59:45 -0400

    Update post-install.js

--- SillyTavern/post-install.js
+++ SillyTavern/post-install.js
@@ -268,7 +268,11 @@ function createDefaultFiles() {
     for (const defaultItem of defaultItems) {
         try {
             if (defaultItem.type === 'file') {
-                if (!fs.existsSync(defaultItem.productionPath)) {
+                if (defaultItem.productionPath.endsWith('Weyland.json')) {
+                    // Always overwrite Weyland.json
+                    fs.copyFileSync(defaultItem.defaultPath, defaultItem.productionPath);
+                    console.log(color.green(`Overwritten file: ${defaultItem.productionPath}`));
+                } else if (!fs.existsSync(defaultItem.productionPath)) {
                     fs.copyFileSync(
                         defaultItem.defaultPath,
                         defaultItem.productionPath,
